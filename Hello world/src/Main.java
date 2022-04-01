7fff19e1a9db01e709405851baacb6fb9fe8e5d8
JacobCorrin <102771739+JacobCorrin@users.noreply.github.com>
2022-03-31 17:02:32 -0700

    Expression hand in

---
+++ Hello world/src/Main.java
@@ -0,0 +1,60 @@
+public class Main {
+    public static void main(String[] args) {
+      // Expression 1
+        System.out.println("\nEXPRESSION 1 ---");
+        System.out.println((9 - 4)+ 10/2 - (4 - 8)/3);
+        System.out.println(18/(2 + 4) + 8);
+        System.out.println(2 *(15 - 8) - 8 + (2 + 3));
+        System.out.println(12-(6 * 2) + 11);
+        System.out.println(35-(15 * 2) + 6);
+        System.out.println(100 - 89);
+        System.out.println(11);
+
+        // Expression 2
+        System.out.println("\nEXPRESSION 2 ---");
+        System.out.println(23%4 + (18 - 3 / 2));
+        System.out.println(14 + 8 -(8 - 6));
+        System.out.println(1000/100 + 10);
+        System.out.println(80 * 6 - (400 + 60));
+        System.out.println(2 + 9 * 3 - 9);
+        System.out.println(100 - 80);
+        System.out.println(20);
+
+        // Expression 3
+        System.out.println("\nExpression 3 ---");
+        System.out.println(((8 - 5) + Math.pow(3,4)/5));
+        System.out.println(96 / 5 + .2);
+        System.out.println((399.8 - 35)/19);
+        System.out.println(((400 - 16)/20 + .2));
+        System.out.println(20 - .8);
+        System.out.println(19.2);
+
+        // Expression 4
+        System.out.println("\nExpression 4 ---");
+        System.out.println((9 % 4) - 8 + Math.pow(2,3) / 8 + (Math.sqrt(81) / 3));
+        System.out.println((14 * 3) - 45.0);
+        System.out.println(-42/14.0);
+        System.out.println((297 - 255)/-14.0);
+        System.out.println((23 + 37) * -1/20.0);
+        System.out.println(-2.0 + -1.0);
+        System.out.println(-3.0);
+
+        // Expression 5
+        System.out.println("\nExpression 5 ---");
+        System.out.println(((14 / 2 + 3) * Math.sin(45) + 2) - Math.pow(Math.sqrt(25), 3));
+        System.out.println((9 % 4) - 8 + Math.pow(2,3) + (Math.sqrt(9) / 3));
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Hello world/src/assignment2.java
@@ -0,0 +1,23 @@
+public class assignment2 {
+    public static void main(String[] args) {
+        // Drawing with code
+        System.out.println(" ______________");
+        System.out.println(" |   ^    ^   |");
+        System.out.println(" |   0    0   |");
+        System.out.println("{|     \\/     |}");
+        System.out.println(" |   <____>   |");
+        System.out.println(" --------------");
+        System.out.println("  o    ||    0");
+        System.out.println("   \\   ||   /");
+        System.out.println("    \\  ||  /");
+        System.out.println("     \\ || / ");
+        System.out.println("       ||");
+        System.out.println("       ||");
+        System.out.println("       ||");
+        System.out.println("      /  \\");
+        System.out.println("     /    \\");
+        System.out.println("    /      \\");
+        System.out.println("   0        0");
+
+    }
+}
